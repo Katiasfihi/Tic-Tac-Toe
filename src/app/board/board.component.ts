678166aaa0e4de60f72716c33db822df49e01000
Katiasfihi <katia.sfihi@gmail.com>
2021-07-16 16:45:50 +0100

    working on sass and logic

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -20,7 +20,7 @@ export class BoardComponent implements OnInit {
   newGame() {
     this.squares = Array(9).fill(null)
     this.xIsNext = false;
-    this.winner = '';
+    this.winner;
   }
 
   get player() {
@@ -32,6 +32,8 @@ export class BoardComponent implements OnInit {
       this.squares.splice(idx, 1, this.player);
       this.xIsNext = !this.xIsNext
     }
+
+    this.winner = this.calculateWinner();
   }
 
 
