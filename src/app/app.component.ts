7ec4eaf14809cc3680094b803b29639a6ddc20ba
Katiasfihi <katia.sfihi@gmail.com>
2021-07-16 11:42:22 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { KatiaComponent } from './katia/katia.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tic-Tac-Toe';
+  
+
 }
